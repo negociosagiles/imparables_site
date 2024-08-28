637f6509bdf8610d2d0f29fcea0989d78e936322
Danny Montalvo <danny.montalvo@gmail.com>
2024-08-27 21:32:05 -0500

    Website Imparables

---
+++ README.md
@@ -0,0 +1,1 @@
+# imparables_site
---
+++ web/themes/custom/imparables_bb5/js/custom.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.imparables_bb5 = {
+    attach: function(context, settings) {
+
+      // Custom code here
+
+    }
+  };
+
+})(jQuery, Drupal);
